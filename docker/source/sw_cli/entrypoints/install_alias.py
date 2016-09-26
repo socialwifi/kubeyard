e318efd3afe0ab9f8396c82ed7e2f88b007581d6
Jakub Skiepko <jakub.skiepko@socialwifi.com>
2016-09-26 17:03:49 +0200

    Add development setup for sw-cli

--- docker/source/sw_cli/entrypoints/install_alias.py
+++ docker/source/sw_cli/entrypoints/install_alias.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import optparse
 import pathlib
 import sw_cli.files_generator
 
@@ -7,12 +8,22 @@ def run():
     installation_directory = pathlib.Path('/installation')
     if installation_directory.is_dir():
         print("installing sw-cli")
+        options, args = parse_arguments()
         sw_cli_destination = installation_directory / 'sw-cli'
-        sw_cli.files_generator.copy_template('sw-cli.sh', sw_cli_destination)
+        sw_cli.files_generator.copy_template('sw-cli.sh.template', sw_cli_destination, {
+            'development_directory': options.development_directory,
+        })
         sw_cli_destination.chmod(0o755)
         print('done')
     else:
         print('Add option "-v /usr/bin:/installation" to docker run command.')
+
+
+def parse_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("--development-dir", dest="development_directory", help="Installs.")
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
     run()
