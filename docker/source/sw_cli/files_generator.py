94ef9ebe0b3abaebd6d4ce80deb86916d37d5e98
Jakub Skiepko <jakub.skiepko@socialwifi.com>
2016-09-26 10:33:12 +0200

    Refactor.

--- docker/source/sw_cli/files_generator.py
+++ docker/source/sw_cli/files_generator.py
@@ -2,21 +2,24 @@ import contextlib
 import pathlib
 import shutil
 
+import collections
+
 templates_directory = pathlib.Path(__file__).parent / 'templates'
 
 
 def copy_template(template, destination):
     source = templates_directory / template
+    environment = CopyEnvironment(source, destination)
     for path in traverse(source):
-        path_destination = destination / path.relative_to(source)
         if path.is_dir():
-            with contextlib.suppress(FileExistsError):
-                path_destination.mkdir()
+            DirectoryTemplate(path, environment).render()
         else:
-            if path_destination.exists():
-                print('file {} already exists. Skipping.'.format(str(path_destination)))
+            file_template = FileTemplate(path, environment)
+            if file_template.destination.exists():
+                print('file {} already exists. Skipping.'.format(str(file_template.destination)))
             else:
-                shutil.copy(str(path), str(path_destination))
+                file_template.render()
+
 
 
 def traverse(path : pathlib.Path):
@@ -24,3 +27,30 @@ def traverse(path : pathlib.Path):
     if path.is_dir():
         for sub in path.iterdir():
             yield from traverse(sub)
+
+CopyEnvironment = collections.namedtuple('CopyEnvironment', ['source_root', 'destination_root'])
+
+
+class BaseTemplate:
+    def __init__(self, source, environment):
+        self.source = source
+        self.environment = environment
+
+    @property
+    def destination(self):
+        relative_path = self.source.relative_to(self.environment.source_root)
+        return self.environment.destination_root / relative_path
+
+    def render(self):
+        raise NotImplementedError
+
+
+class DirectoryTemplate(BaseTemplate):
+    def render(self):
+        with contextlib.suppress(FileExistsError):
+            self.destination.mkdir()
+
+
+class FileTemplate(BaseTemplate):
+    def render(self):
+        shutil.copy(str(self.source), str(self.destination))
