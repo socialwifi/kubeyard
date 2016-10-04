39d6db99aa35eb30bca48b7f0faadb39543ca265
Jakub Skiepko <jakub.skiepko@socialwifi.com>
2016-10-04 16:08:30 +0200

    Install templates along with code.

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 from pip.req import parse_requirements
 from setuptools import find_packages
 from setuptools import setup
+import pathlib
+
+
+def templates():
+    for path in pathlib.Path('sw_cli/templates').glob('**/*'):
+        if not path.is_dir():
+            yield str(path.relative_to('sw_cli'))
 
 
 setup(
@@ -14,4 +21,7 @@ setup(
             'sw-cli = sw_cli.entrypoints.sw_cli:run',
         ],
     },
+    package_dir={'sw_cli': 'sw_cli'},
+    package_data={'sw_cli': list(templates())},
+    include_package_data=True,
 )
