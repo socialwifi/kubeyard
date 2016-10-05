be6445918ebb36a20858589995f579a0eb26b280
Jakub Skiepko <jakub.skiepko@socialwifi.com>
2016-10-05 11:23:03 +0200

    Refactor.

--- sw_cli/commands/devel.py
+++ sw_cli/commands/devel.py
@@ -1,16 +1,12 @@
 import os
 
 import sh
-from cached_property import cached_property
 
 from sw_cli import base_command
-from sw_cli.base_command import CommandException
-
-DOCKER_ENV_KEYS = ['DOCKER_TLS_VERIFY', 'DOCKER_HOST', 'DOCKER_CERT_PATH', 'DOCKER_API_VERSION']
+from sw_cli import minikube
 
 
 class DevelCommand(base_command.BaseCommand):
-
     def __init__(self):
         super(DevelCommand, self).__init__()
         self._prepare_minikube()
@@ -24,24 +20,14 @@ class DevelCommand(base_command.BaseCommand):
     def deploy(self):
         self._run_script('deploy')
 
-    @cached_property
-    def docker_env(self):
-        variables = map(lambda x: '$' + x, DOCKER_ENV_KEYS)
-        result = sh.bash('-c', 'eval $(minikube docker-env); echo "%s"' % '|'.join(variables))
-        values = result.strip("\n").split('|')
-        return dict(zip(DOCKER_ENV_KEYS, values))
-
     def _prepare_minikube(self):
-        status = sh.minikube('status')
-        if status.strip().lower() == 'stopped':
-            print("Starting minikube...")
-            sh.minikube('start')
-        self.context.update(self.docker_env)
+        minikube.ensure_minikube_started()
+        self.context.update(minikube.docker_env())
 
     def _run_script(self, script_name):
         filepath = self.project_dir / self.context.get('SWCLI_DEVEL_%s_SCRIPT_PATH' % script_name.upper())
         if not filepath.exists():
-            raise CommandException("Could not execute %s command, file doesn't exist: %s" % (script_name, filepath))
+            raise base_command.CommandException("Could not execute %s command, file doesn't exist: %s" % (script_name, filepath))
         env = os.environ.copy()
         env.update(self.context)
         script = sh.Command(str(filepath))
---
+++ sw_cli/minikube.py
@@ -0,0 +1,20 @@
+import functools
+
+import sh
+
+DOCKER_ENV_KEYS = ['DOCKER_TLS_VERIFY', 'DOCKER_HOST', 'DOCKER_CERT_PATH', 'DOCKER_API_VERSION']
+
+
+def ensure_minikube_started():
+    status = sh.minikube('status')
+    if status.strip().lower() == 'stopped':
+        print("Starting minikube...")
+        sh.minikube('start')
+
+
+@functools.lru_cache(maxsize=1)
+def docker_env():
+    variables = map(lambda x: '$' + x, DOCKER_ENV_KEYS)
+    result = sh.bash('-c', 'eval $(minikube docker-env); echo "%s"' % '|'.join(variables))
+    values = result.strip("\n").split('|')
+    return dict(zip(DOCKER_ENV_KEYS, values))
