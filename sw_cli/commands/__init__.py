fe8336c20532b08380076eeba174c9b50129878d
Jakub Skiepko <jakub.skiepko@socialwifi.com>
2016-10-06 16:35:14 +0200

    Fix init commands.

--- sw_cli/commands/__init__.py
+++ sw_cli/commands/__init__.py
@@ -37,8 +37,9 @@ def get_all_commands():
     def cmd_exists(name):
         return any(cmd.name == name for cmd in cmds)
 
-    scripts_dir = pathlib.Path(settings.DEFAULT_SWCLI_SCRIPTS_DIR).resolve()
+    scripts_dir = pathlib.Path(settings.DEFAULT_SWCLI_SCRIPTS_DIR)
     if scripts_dir.exists():
+        scripts_dir = scripts_dir.resolve()
         for filepath in scripts_dir.glob("*"):
             if filepath.is_file() and not cmd_exists(filepath.name):
                 cmds.append(CommandDeclaration(filepath.name, lambda: custom_script.run(filepath.name)))
