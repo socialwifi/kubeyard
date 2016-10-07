7a16932e43262336c1dd6eb53dd65499c61e8cd1
Jakub Skiepko <jakub.skiepko@socialwifi.com>
2016-10-07 15:32:16 +0200

    Add development option to setup command.

--- sw_cli/commands/global_commands.py
+++ sw_cli/commands/global_commands.py
@@ -1,5 +1,7 @@
 import contextlib
 import pathlib
+from optparse import OptionParser
+
 import yaml
 from sw_cli import context_factories, kubernetes
 
@@ -11,6 +13,8 @@ def setup():
 class GlobalCommand(object):
     def __init__(self):
         self.context = context_factories.GlobalContextFactory().get()
+        parser = self.get_parser()
+        self.options, self.args = parser.parse_args()
 
     def setup(self):
         user_context_filepath = pathlib.Path(self.context['SWCLI_USER_CONTEXT_FILEPATH'])
@@ -18,6 +22,12 @@ class GlobalCommand(object):
             user_context_filepath.parent.mkdir()
         with user_context_filepath.open('w') as context_file:
             yaml.dump({
-                'SWCLI_MODE': 'development',
+                'SWCLI_MODE': self.options.mode,
             }, stream=context_file)
         kubernetes.Kubernetes().setup_cluster_context()
+
+    def get_parser(self):
+        parser = OptionParser()
+        parser.add_option("--development", dest="mode", default='production', action='store_const', const='development',
+                          help="Select project root directory.")
+        return parser
