cf00fb228a63b5b05de272893fae75c941c9a99d
adam.przywarty <adam.przywarty@socialwifi.com>
2017-06-02 13:26:57 +0200

    Refactor.

--- sw_cli/commands/jenkins.py
+++ sw_cli/commands/jenkins.py
@@ -18,13 +18,10 @@ class JenkinsCommand(base_command.BaseCommand):
         password = getpass.getpass(prompt='Password: ', stream=None)
         return username, password
 
-    def get_config_xml(self):
-        filename = self.project_dir / self.context.get('JENKINS_JOB_CONFIG_FILEPATH',
-                                                       settings.DEFAULT_JENKINS_JOB_CONFIG_FILEPATH)
-
+    def get_config_xml(self, config_filepath):
+        filename = self.project_dir / config_filepath
         if not filename.exists():
             raise base_command.CommandException("Could not find jenkins job configuration: %s. Exiting." % filename)
-
         template = files_generator.jinja_environment.get_template(str(filename))
         return template.render(self.context)
 
@@ -34,12 +31,14 @@ class JenkinsCommand(base_command.BaseCommand):
     def init_job(self):
         job_name = self.context['DOCKER_IMAGE_NAME']
         print("Initialising jenkins job: %s" % job_name)
-        self.server.create_job(job_name, self.get_config_xml())
+        config_filepath = self.context.get('JENKINS_JOB_CONFIG_FILEPATH', settings.DEFAULT_JENKINS_JOB_CONFIG_FILEPATH)
+        self.server.create_job(job_name, self.get_config_xml(config_filepath))
 
     def reconfig_job(self):
         job_name = self.context['DOCKER_IMAGE_NAME']
         print("Reconfiguring jenkins job: %s" % job_name)
-        self.server.reconfig_job(job_name, self.get_config_xml())
+        config_filepath = self.context.get('JENKINS_JOB_CONFIG_FILEPATH', settings.DEFAULT_JENKINS_JOB_CONFIG_FILEPATH)
+        self.server.reconfig_job(job_name, self.get_config_xml(config_filepath))
 
     def build_job(self):
         job_name = self.context['DOCKER_IMAGE_NAME']
