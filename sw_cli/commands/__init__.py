a426852999172c91fb6325c06083d7307e89a591
Mateusz Legięcki <mateusz.legiecki@socialwifi.com>
2018-11-16 12:20:41 +0100

    Refactor: move update requirements command to separate file.

--- sw_cli/commands/__init__.py
+++ sw_cli/commands/__init__.py
@@ -4,7 +4,7 @@ from sw_cli.commands.debug import DebugCommand
 from sw_cli.commands.build import BuildCommand
 from sw_cli.commands.deploy import DeployCommand
 from sw_cli.commands.push import PushCommand
-from sw_cli.commands.devel import UpdateRequirementsCommand
+from sw_cli.commands.update_requirements import UpdateRequirementsCommand
 from sw_cli.commands.fix_code_style import FixCodeStyle
 from sw_cli.commands.global_commands import InstallGlobalSecretsCommand
 from sw_cli.commands.global_commands import SetupCommand
