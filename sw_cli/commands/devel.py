a75687a95a3e716374fdc64467a205d1c1b42db5
Mateusz Legięcki <mateusz.legiecki@socialwifi.com>
2018-11-16 12:06:09 +0100

    Refactor: move build command to separate file.

--- sw_cli/commands/devel.py
+++ sw_cli/commands/devel.py
@@ -106,24 +106,6 @@ class BaseDevelCommand(base_command.InitialisedRepositoryCommand):
         raise NotImplementedError
 
 
-class BuildCommand(BaseDevelCommand):
-    """
-    Builds docker image required to run tests and deployment. Can be overridden in <project_dir>/sripts/build.
-    If sw-cli is set up in development mode it uses minikube as docker host.
-    """
-    custom_script_name = 'build'
-    context_vars = ['image_context']
-
-    def __init__(self, *, image_context, **kwargs):
-        super().__init__(**kwargs)
-        self.image_context = image_context
-
-    def run_default(self):
-        image_context = self.image_context or "{0}/docker".format(self.project_dir)
-        logger.info('Building image "{}"...'.format(self.image))
-        self.docker_with_output('build', '-t', self.image, image_context)
-
-
 class UpdateRequirementsCommand(BaseDevelCommand):
     """
     Updates docker/requirements/python.txt file based on docker/source/base_requirements.txt.
