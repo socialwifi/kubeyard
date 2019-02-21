1282849046a8d2c2b7cf1b64664bd0b7fb23c086
Tomek Glowka <tk.glowka@gmail.com>
2019-02-21 23:59:59 +0100

    Add optional NAME argument to variables command

--- kubeyard/commands/debug.py
+++ kubeyard/commands/debug.py
@@ -6,7 +6,16 @@ from kubeyard import base_command
 class DebugCommand(base_command.InitialisedRepositoryCommand):
     """
     Prints current context.
+
+    If NAME supplied prints value of the single variable instead of dict containing whole context.
     """
+    def __init__(self, name, **kwargs):
+        super().__init__(**kwargs)
+        self.name = name
+
     def run(self):
         super().run()
-        pprint(self.context)
+        if self.name is not None:
+            print(self.context.get(self.name.upper()))
+        else:
+            pprint(self.context)
