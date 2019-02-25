5ee41461647137902d746068d05ea084c4d60c8e
Mateusz Legięcki <mateusz.legiecki@socialwifi.com>
2019-02-25 10:03:33 +0100

    Fix import order.

--- kubeyard/commands/__init__.py
+++ kubeyard/commands/__init__.py
@@ -1,16 +1,16 @@
 from kubeyard.commands.bash_completion import InstallCompletion
+from kubeyard.commands.build import BuildCommand
 from kubeyard.commands.custom_script import CustomScriptCommand
 from kubeyard.commands.debug import DebugCommand
-from kubeyard.commands.build import BuildCommand
 from kubeyard.commands.deploy import DeployCommand
-from kubeyard.commands.shell import ShellCommand
-from kubeyard.commands.push import PushCommand
-from kubeyard.commands.update_requirements import UpdateRequirementsCommand
 from kubeyard.commands.fix_code_style import FixCodeStyleCommand
 from kubeyard.commands.global_commands import InstallGlobalSecretsCommand
 from kubeyard.commands.global_commands import SetupCommand
 from kubeyard.commands.init import InitCommand
+from kubeyard.commands.push import PushCommand
+from kubeyard.commands.shell import ShellCommand
 from kubeyard.commands.test import TestCommand
+from kubeyard.commands.update_requirements import UpdateRequirementsCommand
 
 __all__ = [
     InstallCompletion,
