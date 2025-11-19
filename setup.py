92d3204e94a8185535500b6e984ea508282c8d25
Jakub Gocławski <jakub@goclawski.net>
2025-11-19 14:31:18 +0100

    Back to development: 1.2.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def get_long_description():
 
 setup(
     name='kubeyard',
-    version='1.2.0',
+    version='1.2.1.dev0',
     description='A utility to develop, test and deploy Kubernetes microservices.',
     long_description=get_long_description(),
     long_description_content_type='text/markdown',
