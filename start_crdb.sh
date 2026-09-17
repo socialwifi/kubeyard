b784191ecfc0acbb3a2a782df5d51e8ebbbd2e46
Jakub Gocławski <jakub.goclawski@socialwifi.com>
2026-09-17 15:02:57 +0200

    Remove unnecessary file.

--- start_crdb.sh
+++
@@ -1,8 +0,0 @@
-#start-single-node --insecure --host=localhost --logtostderr
-
-
-docker run -d \
-  --name=roach-single \
-  cockroachdb/cockroach:v22.2.19 start-single-node \
-  --insecure \
-  --host=localhost
